3061f188db84db7a0f0107d9d0dbdc1d80957b9c
Christian Kim <cykim@cpp.edu>
2017-04-25 16:22:33 -0700

    Client Working

--- Ex3Client.java
+++ Ex3Client.java
@@ -1,45 +1,79 @@
-import java.net.*;
 import java.io.*;
 import java.util.*;
+import java.net.*;
 
-public class Ex3Client{
+public class Ex3Client {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
 
-        try(Socket socket = new Socket("codebank.xyz",38102)){
+        try (Socket socket = new Socket("codebank.xyz", 38103)) {
             System.out.println("Connected to Server.");
             InputStream is = socket.getInputStream();
-            InputStreamReader isr = new InputStreamReader(is, "UTF-8");
-            BufferedReader br = new BufferedReader(isr);
             OutputStream out = socket.getOutputStream();
-			PrintWriter pw = new PrintWriter(out);
 
-            int fh,sh,numVal;
-            fh = is.read();
-            sh = is.read();
-            numVal = 16*fh+sh;
+            int numVal = is.read();
+            System.out.println("Reading " + numVal + " bytes.");
             byte[] bArray = new byte[numVal];
+            System.out.print("Data Received:");
 
-            System.out.printf("Reading %d byte(s).",numVal);
-            int data;
-            for(int i = 0; i < numVal;i++){
-                fh = is.read();
-                sh = is.read();
-                data = 16*fh+sh;
-                bArray[i] = (byte) data;
+            String byteString;
+            for (int i = 0; i < numVal; i++) {
+                byteString = "";
                 if(i%10==0){
-                    System.out.println("\t\t");
+                    System.out.print("\n    ");
                 }
-                System.out.print((Integer.toHexString(fh)).toUpperCase() + (Integer.toHexString(sh)).toUpperCase());
+                int data = is.read();
+                byteString += String.format("%02X",data);                
+                System.out.print(byteString);
+                bArray[i] = (byte) data;
             }
-            System.out.println("\ndone");
-            
 
+            System.out.println();
+            short cksum = checksum(bArray);
+            System.out.println("Checksum Calculated: 0x" + String.format("%04X", cksum & 0xFFFF));
 
-        } catch(Exception e){
+            byte[] toServer = new byte[2];
+            toServer[0] = (byte) ((cksum & 0xFF00) >>> 8);
+            toServer[1] = (byte) ((cksum & 0x00FF));
+            out.write(toServer);
 
+            int response = is.read();
+            if (response == 1){
+                System.out.println("Response Good.");
+            }
+            else{
+                System.out.println("Response Bad.");
+            }
+            System.out.println("Disconnected from Server.");
+
+        } catch (Exception e) {
         }
-
     }
 
+
+    public static short checksum(byte[] b) {
+        int l = b.length;
+        int i = 0;
+        long total = 0;
+        long sum = 0;
+
+        while (l > 1) {
+            sum = sum + ((b[i] << 8 & 0xFF00) | ((b[i + 1]) & 0x00FF));
+            i = i + 2;
+            l = l - 2;
+            if ((sum & 0xFFFF0000) > 0) {
+                sum = sum & 0xFFFF;
+                sum++;
+            }
+        }
+        if (l > 0) {
+            sum += b[i] << 8 & 0xFF00;
+            if ((sum & 0xFFFF0000) > 0) {
+                sum = sum & 0xFFFF;
+                sum++;
+            }
+        }
+        total = (~((sum & 0xFFFF) + (sum >> 16))) & 0xFFFF;
+        return (short) total;
+    }
 }
